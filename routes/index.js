01ebfed05714a43794836ac20e102dc32751afc3
Zafk1eI <sintfrank@gmail.com>
2023-11-21 16:02:23 +0300

    change routers

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,35 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/Nightbringer', function(req, res, next) {
-  res.send("<h1>Страница Nightbringer Yasuo</h1>")
+router.get('/Nightbringer', function (req, res, next) {
+  res.render('skin', {
+    title: "Nightbringer Yasuo",
+    picture: "images/Nightbringer.jpg",
+    desc: "Ясуо, рожденный из гноящихся отголосков на заре творения, является адским воплощением хаоса в космосе. Обреченный навсегда сразиться с Несущим Рассвет, он ждет дня, когда его тьма наконец рассечет ее свет."
+  });
 });
 
-router.get('/SeaDog', function(req, res, next) {
-  res.send("<h1>Страница Sea Dog Yasuo</h1>")
+
+router.get('/seadog', function (req, res, next) {
+  res.render('skin', {
+    title: "Sea Dog yasuo",
+    picture: "images/SeaDog.jpg",
+    desc: "Получив силу от Бухру во время битвы против Вьего из Камавора, Ясуо стал маловероятным защитником для детей Нагакабуроса — его одержимость прошлыми событиями шла вразрез со всем, во что они верили. силы тьмы одерживают победу."
+  });
 });
 
-router.get('/TruthDragon', function(req, res, next) {
-    res.send("<h1>Страница Truth Dragon Yasuo</h1>")
+
+router.get('/truthdragon', function (req, res, next) {
+  res.render('skin', {
+    title: "Truth Dragon",
+    picture: "images/TruthDragon.jpg",
+    desc: "В одиночку против армии Ясуо решил выстоять и сражаться. Он заблокировал узкий проход между двумя горами, позволив невинным избежать кровопролития. Владея истиной стали как танцем, он сразил бесчисленное количество врагов, но даже он не смог продержаться вечно. Дракон Истины, движимый своим искусством фехтования, спустился со своей вершины, чтобы благословить его…"
+  });
 });
+
 
 module.exports = router;
