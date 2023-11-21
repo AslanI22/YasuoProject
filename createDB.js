38007052952a36a694e626192829a32edb9bd6c9
Zafk1eI <sintfrank@gmail.com>
2023-11-21 16:41:22 +0300

    bd

---
+++ createDB.js
@@ -0,0 +1,17 @@
+var MongoClient = require('mongodb').MongoClient
+const uri = "mongodb://127.0.0.1:27017"
+const client = new MongoClient(uri)
+async function run() {
+try {
+await client.connect();
+var database = client.db("myYasuo");
+database.dropDatabase()
+database = client.db("myYasuo");
+const cats = database.collection("skins");
+const result = await cats.insertOne({name:"Nightbringer Yasuo"});
+console.log(`${result} documents were inserted`);
+} finally {
+await client.close();
+}
+}
+run();
