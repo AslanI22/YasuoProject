524df3bc1fc9fe39db5da69248767e2613dc5307
Zafk1eI <sintfrank@gmail.com>
2023-11-21 15:41:15 +0300

    roters

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/Nightbringer', function(req, res, next) {
+  res.send("<h1>Страница Nightbringer Yasuo</h1>")
+});
+
+router.get('/SeaDog', function(req, res, next) {
+  res.send("<h1>Страница Sea Dog Yasuo</h1>")
+});
+
+router.get('/TruthDragon', function(req, res, next) {
+    res.send("<h1>Страница Truth Dragon Yasuo</h1>")
+});
+
 module.exports = router;
