3f5af0dd6a2e88bb6a2b8b4235219f930c264944
AslanI22 <reshetov.aslan.i22@gmail.com>
2023-12-03 21:23:53 +0300

    fix

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,8 +1,16 @@
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/test')
-var Yas = mongoose.model('Yasuo', { name: String })
+var schema = mongoose.Schema({ name: String })
+
+
+schema.methods.fidim = function(){
+    console.log(this.get("name") + " жестко нафидил")
+    }
+
+var Yas = mongoose.model('Yasuo', schema);
 var fid = new Yas({ name: 'Nightbringer' })
 fid.save().then(() => {
+    fid.fidim();
     console.log('Хасаги!');
 })
 .catch((err) => {
