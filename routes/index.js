0b38bf6b835da25b5a75af17bd628de9b8112e09
AslanI22 <reshetov.aslan.i22@gmail.com>
2023-12-19 20:25:29 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+var Yasuo = require('../models/yasuo.js').Yasuo
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', {title: 'Express'});
+
+router.get('/', async (req, res, next) => {
+  try {
+    const menu = await Yasuo.find({}, { _id: 0, title: 1, nick: 1 });
+    res.render('index', {
+      title: 'Express',
+      menu: menu
+    });
+  } catch (err) {
+    next(err);
+  }
 });
 
 module.exports = router;
--- routes/yasoes.js
+++ routes/yasoes.js
@@ -1,21 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var Yasuo = require('../models/yasuo.js').Yasuo
+var Yasuo = require('../models/yasuo.js').Yasuo;
+var async = require("async")
 
 router.get('/:nick', async function(req, res, next) {
     try {
-        const myYasuo = await Yasuo.findOne({ nick: req.params.nick }).exec();
-        if (!myYasuo) {
-            throw new Error("такого yasuo пока нет на сайте");
-        }
-        res.render('yasuo', {
-            title: myYasuo.title,
-            picture: myYasuo.avatar,
-            desc: myYasuo.desc,
-        });
+      const [yasuo, yasuos] = await Promise.all([
+        Yasuo.findOne({ nick: req.params.nick }),
+        Yasuo.find({}, { _id: 0, title: 1, nick: 1 })
+      ]);
+    
+      if (!yasuo) {
+        throw new Error("Нет такого");
+      }
+      
+      renderYasuo(res, yasuo.title, yasuo.avatar, yasuo.desc, yasuos);
     } catch (err) {
-        return next(err);
+      next(err);
     }
-});
+  });
+  
+  function renderYasuo(res, title, picture, desc, yasuos) {
+    console.log(yasuos);
+  
+    res.render('yasuo', {
+      title: title,
+      picture: picture,
+      desc: desc,
+      menu: yasuos
+    });
+  }
 
 module.exports = router;
