553efda8b748a2e347e769280da261c7f66b9fb0
AslanI22 <reshetov.aslan.i22@gmail.com>
2023-12-24 16:03:56 +0300

    fix

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Yasuo = require('../models/yasuo.js').Yasuo;
+var User = require("../models/User").User
 
 /* GET home page. */
 
@@ -18,8 +19,26 @@ router.get('/logreg', function(req, res, next) {
 });
   
 router.post('/logreg', function(req, res, next) {
-  //var username = req.body.username
-  //var password = req.body.password  
+  var username = req.body.username
+  var password = req.body.password
+  User.findOne({username:username},function(err,user){
+  if(err) return next(err)
+  if(user){
+  if(user.checkPassword(password)){
+  req.session.user = user._id
+  res.redirect('/')
+  } else {
+  res.render('logreg', {title: 'Вход'})
+  }
+  } else {
+  var user = new User({username:username,password:password})
+  user.save(function(err,user){
+  if(err) return next(err)
+  req.session.user = user._id
+  res.redirect('/')
+})
+}
+})
 });
   
 module.exports = router;
