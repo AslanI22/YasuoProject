bfeffffe57e3204d8a22e309b97830f726057627
AslanI22 <reshetov.aslan.i22@gmail.com>
2024-01-13 15:48:30 +0300

    fix

--- routes/yasoes.js
+++ routes/yasoes.js
@@ -1,10 +1,34 @@
 var express = require('express');
 var router = express.Router();
-var Yasuo = require('../models/yasuo.js').Yasuo;
-var async = require("async")
-var checkAuth = require("./../middleware/checkAuth.js")
+var db = require('../mySQLConnect.js');
+//var Yasuo = require('../models/yasuo.js').Yasuo;
+var async = require("async");
+//var checkAuth = require("./../middleware/checkAuth.js")
 
-router.get('/:nick',checkAuth, async function(req, res, next) {
+
+/* GET cats listing. */
+router.get('/', function (req, res, next) {
+  res.send('<h1>Это экран для списка скинов на Ясуо</h1>');
+});
+
+router.get("/:nick", function (req, res, next) {
+  db.query(`SELECT * FROM yasuos WHERE yasuos.nick = '${req.params.nick}'`, (err,
+    yasuos) => {
+    if (err) {
+      console.log(err);
+      if (err) return next(err)
+    } else {
+      if (yasuos.length == 0) return next(new Error("Нет такого Ясуо"))
+      var yasuo = yasuos[0];
+      res.render('yasuo', {
+        title: yasuo.title,
+        picture: yasuo.avatar,
+        desc: yasuo.about
+      })
+    }
+  })
+});
+/* router.get('/:nick',checkAuth, async function(req, res, next) {
   try {
       const [yasuo, yasuos] = await Promise.all([
         Yasuo.findOne({ nick: req.params.nick })
@@ -27,6 +51,6 @@ router.get('/:nick',checkAuth, async function(req, res, next) {
       desc: desc,
       menu: yasuos
     });
-  }
+  } */
 
 module.exports = router;
