7239f7b531fe02f6d25fff52ed02e21793bf2a7a
AslanI22 <reshetov.aslan.i22@gmail.com>
2024-01-13 16:58:01 +0300

    fix

--- middleware/createUser.js
+++ middleware/createUser.js
@@ -1,8 +1,25 @@
-const User = require("../models/User.js").User;
+//const User = require("../models/User.js").User;
 
-module.exports = async function(req, res, next) {
+var db = require('../mySQLConnect.js');
+
+module.exports = function (req, res, next) {
   res.locals.user = null
-  /*try {
+  console.log('Проверяем сессию')
+  console.log(req.session.user)
+  db.query(`SELECT * FROM user WHERE user.id = '${req.session.user}'`, (err,
+    users) => {
+    if (err)
+      return next(err)
+    if (users.length > 0) {
+      res.locals.user = users[0];
+    }
+    next()
+  })
+}
+
+/*module.exports = async function(req, res, next) {
+  res.locals.user = null
+  try {
     const user = await User.findById(req.session.user).exec();
     if (!user) {
       res.locals.user = null;
@@ -12,6 +29,6 @@ module.exports = async function(req, res, next) {
     next();
   } catch (err) {
     next(err);
-  }*/
+  }
   next();
-};
+};*/
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 var checkAuth = require("./../middleware/checkAuth.js")
-const Yasuo = require('../models/yasuo.js').Yasuo;
-var User = require("../models/User").User
-
+var db = require('../mySQLConnect.js');
 /* GET home page. */
 
 router.get('/', async (req, res, next) => {
@@ -15,39 +13,41 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-router.get('/logreg', async function(req, res, next) {
-  res.render('logreg', { title: 'Вход',error:null}); 
-});
-  
-router.post('/logreg', async function(req, res, next) {
-  const username = req.body.username;
-  const password = req.body.password;
-  try {
-      const user = await User.findOne({ username });
-      
-      if (user) {
-          if (user.checkPassword(password)) {
-              req.session.user = user._id;
-              res.redirect('/');
-          } else {
-              res.render('logreg', { title: 'Вход', error: 'Неверный пароль' });
-          }
-      } else {
-          const newUser = new User({ username, password });
-          await newUser.save();
-          req.session.user = newUser._id;
-          res.redirect('/');
-      }
-  } catch (err) {
-      next(err);
-  }
+/* GET login/registration page. */
+router.get('/logreg', async function (req, res, next) {
+  res.render('logreg', { title: 'Вход', error: null });
 });
 
-router.post('/logout', function(req, res, next) {
+router.post('/logreg', async function (req, res, next) {
+  const username = req.body.username;
+  const password = req.body.password;
+  db.query(`SELECT * FROM user WHERE user.username = '${req.body.username}'`,
+    function (err, users) {
+      if (err) return next(err)
+      if (users.length > 0) {
+        var user = users[0];
+        if (password == user.password) {
+          req.session.user = user.id
+          res.redirect('/')
+        } else {
+          res.render('logreg', { title: 'Вход' })
+        }
+      } else {
+        db.query(`INSERT INTO user (username, password) VALUES ('${username}',
+'${password}')`, function (err, user) {
+          if (err) return next(err)
+          req.session.user = user.id
+          res.redirect('/')
+        })
+      }
+    })
+});
+
+/* POST logout. */
+router.post('/logout', function (req, res, next) {
   req.session.destroy()
   res.locals.user = null
   res.redirect('/')
 });
-  
 
 module.exports = router;
